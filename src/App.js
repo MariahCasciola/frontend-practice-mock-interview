d85d97fecb047815ea7852486a644c6e7fbe1c03
MariahCasciola <117303840+MariahCasciola@users.noreply.github.com>
2023-01-21 19:09:28 -0600

    first commit for fresh start

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import PostDetail from "./PostDetail";
 
 function App() {
   const [posts, setPosts] = useState([]);
-
+  
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
       .then((response) => response.json())
       .then(setPosts)
       .catch((error) => {
-        console.log(error);
+        console.log(error)
       });
   }, []);
-  // console.log(posts);
+
   return (
     <div className="App">
-      {posts.map((post, index) => {
-        return <PostDetail key={index} post={post} />;
-      })}
     </div>
   );
 }
